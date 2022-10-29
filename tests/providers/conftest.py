fe16a5d5e3a438b4d2526356a2da10b85ad7bac4
Tom Carrio <tom@carrio.dev>
2022-10-29 13:24:34 -0400

    test: include fixture for flagd provider
    Signed-off-by: Tom Carrio <tom@carrio.dev>

---
+++ tests/providers/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+from open_feature import open_feature_api as api
+from open_feature_contrib.providers.flagd import FlagDProvider
+
+@pytest.fixture()
+def flagd_provider_client():
+    api.set_provider(FlagDProvider())
+    return api.get_client()
