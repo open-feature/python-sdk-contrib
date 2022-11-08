edf6ba1dc34556ed46ef909c2a55a78abee6e143
Tom Carrio <tom@carrio.dev>
2022-11-07 22:59:22 -0500

    style: apply styling, consolidate line length config for isort/flake8, ignore venv
    Signed-off-by: Tom Carrio <tom@carrio.dev>

--- tests/providers/conftest.py
+++ tests/providers/conftest.py
@@ -1,7 +1,8 @@
 import pytest
-
 from open_feature import open_feature_api as api
+
 from open_feature_contrib.providers.flagd import FlagDProvider
+
 
 @pytest.fixture()
 def flagd_provider_client():
