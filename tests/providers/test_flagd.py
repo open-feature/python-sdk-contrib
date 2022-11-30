af4e9c07ac0f2bf62d0dd9ff6cbccae6c439b2de
Tom Carrio <tom@carrio.dev>
2022-11-29 23:23:26 -0500

    refactor: test cases separation of given/when/then bdd cases
    Signed-off-by: Tom Carrio <tom@carrio.dev>

--- tests/providers/test_flagd.py
+++ tests/providers/test_flagd.py
@@ -13,8 +13,11 @@ def setup():
 
 def test_should_get_boolean_flag_from_flagd(flagd_provider_client):
     # Given
+    client = flagd_provider_client
+
     # When
-    flag = flagd_provider_client.get_boolean_details(flag_key="Key", default_value=True)
+    flag = client.get_boolean_details(flag_key="Key", default_value=True)
+
     # Then
     assert flag is not None
     assert flag.value
@@ -23,8 +26,11 @@ def test_should_get_boolean_flag_from_flagd(flagd_provider_client):
 
 def test_should_get_integer_flag_from_flagd(flagd_provider_client):
     # Given
+    client = flagd_provider_client
+
     # When
-    flag = flagd_provider_client.get_integer_details(flag_key="Key", default_value=100)
+    flag = client.get_integer_details(flag_key="Key", default_value=100)
+
     # Then
     assert flag is not None
     assert flag.value == 100
@@ -33,8 +39,11 @@ def test_should_get_integer_flag_from_flagd(flagd_provider_client):
 
 def test_should_get_float_flag_from_flagd(flagd_provider_client):
     # Given
+    client = flagd_provider_client
+
     # When
-    flag = flagd_provider_client.get_float_details(flag_key="Key", default_value=100)
+    flag = client.get_float_details(flag_key="Key", default_value=100)
+
     # Then
     assert flag is not None
     assert flag.value == 100
@@ -43,10 +52,13 @@ def test_should_get_float_flag_from_flagd(flagd_provider_client):
 
 def test_should_get_string_flag_from_flagd(flagd_provider_client):
     # Given
+    client = flagd_provider_client
+
     # When
-    flag = flagd_provider_client.get_string_details(
+    flag = client.get_string_details(
         flag_key="Key", default_value="String"
     )
+
     # Then
     assert flag is not None
     assert flag.value == "String"
@@ -55,15 +67,18 @@ def test_should_get_string_flag_from_flagd(flagd_provider_client):
 
 def test_should_get_object_flag_from_flagd(flagd_provider_client):
     # Given
+    client = flagd_provider_client
     return_value = {
         "String": "string",
         "Number": 2,
         "Boolean": True,
     }
+
     # When
-    flag = flagd_provider_client.get_string_details(
+    flag = client.get_string_details(
         flag_key="Key", default_value=return_value
     )
+
     # Then
     assert flag is not None
     assert flag.value == return_value
