02ed91431c14f9a1a5b565eefdfb1f6502019f76
Tom Carrio <tom@carrio.dev>
2022-11-29 23:06:03 -0500

    test: implemented tests for flagd provider
    Signed-off-by: Tom Carrio <tom@carrio.dev>

--- tests/providers/conftest.py
+++ tests/providers/conftest.py
@@ -1,10 +1,10 @@
 import pytest
 from open_feature import open_feature_api as api
 
-from open_feature_contrib.providers.flagd import FlagDProvider
+from open_feature_contrib.providers.flagd import FlagdProvider
 
 
 @pytest.fixture()
 def flagd_provider_client():
-    api.set_provider(FlagDProvider())
+    api.set_provider(FlagdProvider())
     return api.get_client()
