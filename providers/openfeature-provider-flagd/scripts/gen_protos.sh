8323306c6f5b77456c76364bea5d134cde8a64c2
Federico Bond <federicobond@gmail.com>
2024-02-22 18:40:21 -0300

    fix: include proto file in build
    Signed-off-by: Federico Bond <federicobond@gmail.com>

--- providers/openfeature-provider-flagd/scripts/gen_protos.sh
+++ providers/openfeature-provider-flagd/scripts/gen_protos.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-buf generate buf.build/open-feature/flagd --template schemas/protobuf/buf.gen.python.yaml --output schemas
-rm -rf openfeature/contrib/provider/flagd/proto
-sed -i.bak 's/^from schema.v1 import/from . import/' proto/python/schema/v1/*.py
-rm proto/python/schema/v1/*.bak
-mv proto/python src/openfeature/contrib/provider/flagd/proto
-rmdir proto
+buf generate buf.build/open-feature/flagd --template buf.gen.python.yaml
+sed -i.bak 's/^from schema.v1 import/from . import/' proto/schema/v1/*.py
+rm proto/schema/v1/*.bak
