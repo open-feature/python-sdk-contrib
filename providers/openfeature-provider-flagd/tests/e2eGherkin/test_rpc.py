7c809e80bc1ef3863a722b028fc12657ffd655a4
Simon Schrottner <simon.schrottner@dynatrace.com>
2024-11-10 21:51:34 +0100

    fixup: make tests run, only 4 tests are missing now
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>

--- providers/openfeature-provider-flagd/tests/e2eGherkin/test_rpc.py
+++ providers/openfeature-provider-flagd/tests/e2eGherkin/test_rpc.py
@@ -1,40 +1,39 @@
 import pytest
 from pytest_bdd import scenarios
 from testcontainers.core.container import DockerContainer
+from tests.e2eGherkin.flagd_container import FlagDContainer
 from tests.e2eGherkin.steps import wait_for
 
 from openfeature import api
-from openfeature.client import ProviderStatus
 from openfeature.contrib.provider.flagd import FlagdProvider
 from openfeature.contrib.provider.flagd.config import ResolverType
+from openfeature.provider import ProviderStatus
+
+container: DockerContainer = FlagDContainer()
 
 
-@pytest.fixture(autouse=True, scope="module")
+@pytest.fixture(autouse=True, scope="package")
 def setup(request):
     # Setup code
-    with DockerContainer("ghcr.io/open-feature/flagd-testbed:v0.5.6").with_bind_ports(
-        8013
-    ) as container:
-        container.start()
-        api.set_provider(
-            FlagdProvider(
-                resolver_type=ResolverType.GRPC,
-                port=int(container.get_exposed_port(8013)),
-            )
+    c = container.start()
+    api.set_provider(
+        FlagdProvider(
+            resolver_type=ResolverType.GRPC,
+            port=int(container.get_exposed_port(8013)),
         )
-        client = api.get_client()
-        wait_for(lambda: client.get_provider_status() == ProviderStatus.READY)
-        assert client.get_provider_status() == ProviderStatus.READY
+    )
+    client = api.get_client()
+    wait_for(lambda: client.get_provider_status() == ProviderStatus.READY)
 
     def fin():
-        container.stop()
+        c.stop()
 
     # Teardown code
-
     request.addfinalizer(fin)
 
 
 scenarios(
     "../../test-harness/gherkin/flagd.feature",
     "../../test-harness/gherkin/flagd-json-evaluator.feature",
+    "../../spec/specification/assets/gherkin/evaluation.feature",
 )
