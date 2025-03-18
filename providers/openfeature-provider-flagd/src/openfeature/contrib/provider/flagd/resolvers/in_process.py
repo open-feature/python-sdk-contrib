fafd099f07365a7d0032e8215477b51bfe90c01a
Aditya Vallabh Ganti <adityavallabhganti@gmail.com>
2025-03-18 11:48:44 -0500

    fix(flagd): handle falsy target values correctly (#214)
    * fix(flagd): handle falsy target values correctly
    
    Signed-off-by: AdityaVallabh <aganti@abstract.security>
    
    * chore(flagd): run pre-commit reformatting
    
    Signed-off-by: AdityaVallabh <aganti@abstract.security>
    
    * fix(flagd): skip handling 'None'
    
    Signed-off-by: AdityaVallabh <aganti@abstract.security>
    
    * chore(flagd): fix linting
    
    Signed-off-by: AdityaVallabh <aganti@abstract.security>
    
    ---------
    
    Signed-off-by: AdityaVallabh <aganti@abstract.security>

--- providers/openfeature-provider-flagd/src/openfeature/contrib/provider/flagd/resolvers/in_process.py
+++ providers/openfeature-provider-flagd/src/openfeature/contrib/provider/flagd/resolvers/in_process.py
@@ -121,7 +121,7 @@ class InProcessResolver:
             )
 
         variant, value = flag.get_variant(variant)
-        if not value:
+        if value is None:
             raise ParseError(f"Resolved variant {variant} not in variants config.")
 
         return FlagResolutionDetails(
