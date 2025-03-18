3cc2048b94c11f8e04a53e643d1ba8a39c2f0f24
christian.lutnik <christian.lutnik@dynatrace.com>
2025-03-18 13:01:47 +0100

    fix format
    Signed-off-by: christian.lutnik <christian.lutnik@dynatrace.com>

--- providers/openfeature-provider-flagd/tests/test_metadata.py
+++ providers/openfeature-provider-flagd/tests/test_metadata.py
@@ -7,10 +7,11 @@ import pytest
 from openfeature import api
 from openfeature.contrib.provider.flagd import FlagdProvider
 from openfeature.contrib.provider.flagd.config import ResolverType
+from openfeature.contrib.provider.flagd.resolvers.process.flags import (
+    _validate_metadata
+)
 from openfeature.event import EventDetails, ProviderEvent
 from openfeature.exception import ErrorCode, ParseError
-
-from openfeature.contrib.provider.flagd.resolvers.process.flags import _validate_metadata
 
 
 def create_client(file_name):
